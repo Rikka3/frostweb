beaf0cbca5530e7bcb03d3475b18120ae842e0f4
Rikka3 <135252548+Rikka3@users.noreply.github.com>
2023-06-01 22:58:35 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(n) {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (n + slides.length) % slides.length;
+  slides[currentSlide].classList.add('active');
+}
+
+function autoPlay() {
+  showSlide(currentSlide + 1);
+}
+
+setInterval(autoPlay, 2000); // Change slide every 2 seconds
